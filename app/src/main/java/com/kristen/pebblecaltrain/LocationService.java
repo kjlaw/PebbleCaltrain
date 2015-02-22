9f0f2f15388898e1008aa6592358df513ad7cffe
Catherina Xu <catherinaxu@stanford.edu>
2015-02-21 20:21:21 -0800

    tried to implement threads..

--- app/src/main/java/com/kristen/pebblecaltrain/LocationService.java
+++ app/src/main/java/com/kristen/pebblecaltrain/LocationService.java
@@ -3,9 +3,27 @@ package com.kristen.pebblecaltrain;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+
+import java.util.logging.Handler;
 
 public class LocationService extends Service {
+    private Looper mLocationLooper;
+    private ServiceHandler mServiceHandler;
+
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+
+        public void handleMessage(Message msg) {
+
+        }
+    }
+
     public LocationService() {
+
 
     }
 
