c9afff9909c6174749e337df5c59ac3a4d4310f1
Kristen Law <kristenlaw.2014@gmail.com>
2015-02-21 20:04:25 -0800

    created service file

---
+++ app/src/main/java/com/kristen/pebblecaltrain/LocationService.java
@@ -0,0 +1,17 @@
+package com.kristen.pebblecaltrain;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocationService extends Service {
+    public LocationService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
